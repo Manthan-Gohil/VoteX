9de0c55da65d718cfc1b897cc3e2eaa866a074fb
Manthan-Gohil <manthangohil22577@gmail.com>
2025-05-28 16:47:08 +0530

    all clear

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const flash = require('connect-flash');
+const session = require('express-session'); // <-- Add this line
 const {engine} = require('express-handlebars');
 
 require('dotenv').config();
@@ -21,12 +22,18 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
+// Add session middleware BEFORE flash
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'your_secret_key', // Use a strong secret in production!
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.use(flash());
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine", "ejs");
 // app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "views"));
-
 
 app.use(authRoutes);
 app.use(electionRoutes);
